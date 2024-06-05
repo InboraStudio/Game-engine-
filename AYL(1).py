7c84903610ef5a667ccdb8d34e02bf9e56322c7e
Dr Chamyoung <drchamyoung@gmail.com>
2024-06-05 10:42:17 +0530

    1
    1
    
    Co-Authored-By: Aoi しんゆちゃん <121786011+sinyunchan@users.noreply.github.com>

--- AYL(1).py
+++ AYL(1).py
@@ -29,7 +29,7 @@ from torchvision.utils import save_image
 from rlkit.exploration_strategies.base import \
     PolicyWrappedWithExplorationStrategy
 from rlkit.exploration_strategies.gaussian_and_epsilon_strategy import GaussianAndEpsilonStrategy F
-from rlkit.exploration_strategies.ou_strategy import OUStrategy
+from rlkit.exploration_strategies.ou_strategy import OUStrategy 51
 
 import os.path as osp
 from rlkit.core import logger CUDA
